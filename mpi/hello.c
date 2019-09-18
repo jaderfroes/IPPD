ee3c08c308da6c60e6bfeeef3b1f2eed4d0b1574
jaderfroes <jfdsrosa@inf.ufpel.edu.br>
2019-09-18 13:32:27 -0300

    update

--- mpi/hello.c
+++ mpi/hello.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <mpi.h>
 
+    /* Exercicio
+        processo 0 envia valores para os outros processos, 
+        os outros vão multiplicar o valor recebido e devolver
+        para o processo 0, que vai somar todos os valores
+    */
+
+
 int main(int argc, char *argv[]){
+    
     int size, myRank;
+    int valor_inicial, valor_modificado, total, num_proc;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size); // descobrir quantos processos
@@ -11,11 +20,18 @@ int main(int argc, char *argv[]){
     printf("Hello! sou o processo %d de um total de %d\n", myRank, size);
     fflush(stdout); //sincronização para não ter erro nas mensagens
     
+    if(myRank == 0){
+        for(nproc = 1; num_proc < size; ++nrpoc){
+            valor_inicial = num_proc * 10;
+        }
+        MPI_SEND();
+    }
+    else{
+        
+    }
+    
+
     MPI_Finalize();
 
     return 0;
 }
-
-/*
- 
-*/
