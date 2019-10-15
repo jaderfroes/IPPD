a4c5e33b43be9e61ddba3832211bc87679184154
jaderfroes <jfdsrosa@inf.ufpel.edu.br>
2019-10-14 21:46:50 -0300

    merge mpi finalizado

--- mpi/mergeMPI.c
+++ mpi/mergeMPI.c
@@ -3,89 +3,127 @@
 #include <time.h>
 #include <mpi.h>
 
-typedef struct merge_MPI{
-    int num_proc;    
-    int rank;
-    int *vet;
-}mpi_data;
 
 // colocar o subarray da funcao mpi_merge dentro da struct
-#define SIZE 10
+#define SIZE 100
 
 int *generateRandomIntegers(int *vet);
 void printArray(int *vet);
-mpi_data *initData();
-void initMPI();
-void MPI_merge(mpi_data *control_data);
+//mpi_data *initData();
+//void initMPI();
+//void MPI_SCAT(mpi_data *control_data, int *sub_Array);
+//void MPI_GAT(mpi_data *control_data, int *sub_Array);
+//void MPI_FIN();
 
-void merge(int vetor[], int comeco, int meio, int fim) {
+void merge(int vetor[], int vetAux[], int comeco, int meio, int fim) {
     
-    int com1 = comeco, com2 = meio+1, comAux = 0, tam = fim-comeco+1;
+    int com1 = comeco, com2 = meio+1, comAux = comeco, tam = fim-comeco+1, k;
     
-    int *vetAux;
-    vetAux = (int*)malloc(tam * sizeof(int));
 
-    while(com1 <= meio && com2 <= fim){
+    while((com1 <= meio) && (com2 <= fim)){
+
         if(vetor[com1] < vetor[com2]) {
             vetAux[comAux] = vetor[com1];
             com1++;
-        } else {
+        } 
+        else {
             vetAux[comAux] = vetor[com2];
             com2++;
         }
         comAux++;
     }
 
-    while(com1 <= meio){  //Caso ainda haja elementos na primeira metade
-        vetAux[comAux] = vetor[com1];
-        comAux++;
-        com1++;
-    }
+    if(meio < com1) {
+		for(k = com2; k <= fim; k++) {
+			vetAux[comAux] = vetor[k];
+			comAux++;
+			}		
+	}
+    else {
+		for(k = com1; k <= meio; k++) {
+			vetAux[comAux] = vetor[k];
+			comAux++;			
+			}			
+		}
 
-    while(com2 <= fim) {   //Caso ainda haja elementos na segunda metade
-        vetAux[comAux] = vetor[com2];
-        comAux++;
-        com2++;
-    }
+    for(k = comeco; k <= fim; k++){ // coloca os elementos no vetor original
+        vetor[k] = vetAux[k];
+  	}
 
-    for(comAux = comeco; comAux <= fim; comAux++){    //Move os elementos de volta para o vetor original
-        vetor[comAux] = vetAux[comAux-comeco];
-    }
-    
-    free(vetAux);
 }
 
-void mergeSort(int vetor[], int comeco, int fim, int rank){
+void mergeSort(int vetor[], int aux_array[], int comeco, int fim){
     
-    printf("Olá eu sou o processo[%d]\n", rank);
+    // printf("Olá eu sou o processo[%d]\n", rank);
 
     if (comeco < fim) {
         int meio = (fim+comeco)/2;
-        mergeSort(vetor, comeco, meio, rank);
-        mergeSort(vetor, meio+1, fim, rank);    
-        merge(vetor, comeco, meio, fim);
+        mergeSort(vetor, aux_array, comeco, meio);
+        mergeSort(vetor, aux_array, meio+1, fim);    
+        merge(vetor, aux_array, comeco, meio, fim);
     }
 }
 
 int main(int argc, char *argv[]){
 
+    // initMPI(); // inicializa a execução na biblioteca
 
-    initMPI();
-    mpi_data *control_data = initData();
-    MPI_merge(control_data);
+    int i;
+
+    int *array = (int *)malloc(SIZE * sizeof(int));
+
+    srandom(time(NULL));
+    for(i = 0; i < SIZE; i++){ //gera SIZE numeros aleatórios
+        array[i] = (int) random() % SIZE; 
+                 
+    }
+
+    int rank, num_proc;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_proc); // indica o número de processos envolvidos em um comunicador
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Essa função fornece o rank do processo no grupo do comunicador específico 
+
+    int sub_size = SIZE / num_proc; // tamanho dos subvetores que serão distribuídos
+    int *sub_array = (int *) malloc(sub_size * sizeof(int)); // subvetor para distribuir pelos cores
+
+
+    if(rank == 0)
+        printArray(array); 
+
+    MPI_Scatter(array, sub_size, MPI_INT,
+    sub_array, sub_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int *aux_array = (int *)malloc(sub_size * sizeof(int));
+
+    printf("Olá eu sou o processo[%d]\n", rank);
+
+    mergeSort(sub_array, aux_array, 0, sub_size-1);
     
-    if(control_data->rank == 0)
-        printArray(control_data->vet);   
-
-    mergeSort(control_data->vet, 0, SIZE-1, control_data->rank);
-
-    finilizeMPI(control_data);
     
-    if(control_data->rank == 0)
-        printArray(control_data->vet);
+    int *sorted = NULL;
+	if(rank == 0) {
+		sorted = (int *) malloc(SIZE * sizeof(int));	
+	}
 
-    free(control_data->vet);
-    free(control_data);
+    MPI_Gather(sub_array, sub_size, MPI_INT,
+    sorted, sub_size, MPI_INT, 0,
+    MPI_COMM_WORLD);
+
+    if(rank == 0) {
+		
+		int *other_array = malloc(SIZE * sizeof(int));
+		mergeSort(sorted, other_array, 0, (SIZE - 1));
+		
+		printf("Vetor ordenado: ");
+		printArray(sorted);
+
+		free(sorted);
+		free(other_array);
+			
+	}
+
+    MPI_Barrier(MPI_COMM_WORLD); // Sincronização entre processos MPI num grupo
+	MPI_Finalize();
 
     return 0;
 }
@@ -108,48 +146,8 @@ void printArray(int *vet){
     int i;
 
     for(i = 0; i < SIZE; i++){
-            printf("%d ,", vet[i]);     
+            printf("%d ", vet[i]);     
     }
     printf("\n");
 
-}
-
-void MPI_merge(mpi_data *control_data){
-    
-    int subSize = SIZE / control_data->num_proc; // tamanho do vetor particionado
-    int *sub_array = malloc(subSize * sizeof(int));
-
-    MPI_Scatter(control_data->vet, subSize, MPI_INT,
-    sub_array, subSize, MPI_INT, 0,
-    MPI_COMM_WORLD);
-
-
-}
-
-void initMPI(){
-    
-    MPI_Init(NULL, NULL);
-    return;
-}
-
-mpi_data *initData(){
-    mpi_data *ret;
-    
-    ret = malloc(sizeof(mpi_data));
-
-    ret->vet = generateRandomIntegers(ret->vet);
-
-    MPI_Comm_size(MPI_COMM_WORLD, &ret->num_proc); // indica o número de processos envolvidos em um comunicador
-    MPI_Comm_rank(MPI_COMM_WORLD, &ret->rank); // Essa função fornece o rank do processo no grupo do comunicador específico      
-    
-    return ret;
-
-}
-
-void finilizeMPI(control_data){
-    MPI_Gather(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
-    void *recvbuf, int recvcount, MPI_Datatype recvtype, int root,
-    MPI_Comm comm);
-
-    MPI_Finalize();
 }
